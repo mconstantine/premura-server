5ae800beee4de55c9fe6e548e92b9fa0ee700891
Mauro Constantinescu <mauro.constantinescu@gmail.com>
2018-10-30 21:51:29 +0100

    Refactoring.

--- user/make-createUser.js
+++ user/make-createUser.js
@@ -1,10 +1,16 @@
 module.exports = ({ bcrypt, getDb, roles, createError }) => async (req, res, next) => {
-  if (roles.indexOf(req.session.user.role) < roles.indexOf(req.body.role)) {
+  const name = req.body.name
+  const email = req.body.email
+  const password = req.body.password
+  const role = req.body.role
+  const jobRole = req.body.jobRole
+
+  if (roles.indexOf(req.session.user.role) < roles.indexOf(role)) {
     return next(createError(401, 'you cannot register a user with a role higher than yours'))
   }
 
   const collection = (await getDb()).collection('users')
-  const alreadyExistingUser = await collection.findOne({ email: req.body.email })
+  const alreadyExistingUser = await collection.findOne({ email })
 
   if (alreadyExistingUser) {
     delete alreadyExistingUser.email
@@ -15,11 +21,8 @@ module.exports = ({ bcrypt, getDb, roles, createError }) => async (req, res, nex
   }
 
   const result = await collection.insertOne({
-    name: req.body.name,
-    email: req.body.email,
-    password: await bcrypt.hash(req.body.password, 10),
-    role: req.body.role,
-    jobRole: req.body.jobRole,
+    name, email, role, jobRole,
+    password: await bcrypt.hash(password, 10),
     registrationDate: new Date()
   })
 
--- user/make-createUser.test.js
+++ user/make-createUser.test.js
@@ -7,7 +7,7 @@ let insertOneResult = { insertedId: '1234567890abcdef' }
 roles.includes = jest.fn(() => true)
 
 describe('createUser', () => {
-  const completeData = {
+  const data = {
     name: 'John Doe',
     email: 'john.doe@example.com',
     password: 'password',
@@ -31,15 +31,15 @@ describe('createUser', () => {
 
   it('Should check for existing users', async () => {
     findOne.mockClear()
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
-    expect(findOne).toHaveBeenCalledWith({ email: completeData.email })
+    expect(findOne).toHaveBeenCalledWith({ email: data.email })
   })
 
   it("Should check for the logged in user's role", async () => {
     const requestFromLowerRole = Object.assign({}, req)
     requestFromLowerRole.session.user.role = 'maker'
-    requestFromLowerRole.body = Object.assign({}, completeData)
+    requestFromLowerRole.body = Object.assign({}, data)
     requestFromLowerRole.body.role = 'manager'
     await createUser(requestFromLowerRole, res, next)
     expect(next).toHaveBeenLastCalledWith([401, expect.any(String)])
@@ -47,17 +47,17 @@ describe('createUser', () => {
 
   it('Should create a new user', async () => {
     insertOne.mockClear()
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
 
-    const result = Object.assign({}, completeData)
+    const result = Object.assign({}, data)
     delete result.passwordConfirmation
     expect(insertOne).toHaveBeenCalledWith(expect.objectContaining(result))
   })
 
   it('Should save the registration date', async () => {
     insertOne.mockClear()
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
 
     expect(insertOne).toHaveBeenCalledWith(expect.objectContaining({
@@ -67,14 +67,14 @@ describe('createUser', () => {
 
   it('Should encrypt the password', async () => {
     bcrypt.hash.mockClear()
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
-    expect(bcrypt.hash).toHaveBeenCalledWith(completeData.password, 10)
+    expect(bcrypt.hash).toHaveBeenCalledWith(data.password, 10)
   })
 
   it('Should report the conflict in case of existing user', async () => {
     findOneResult = { test: true }
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
     expect(next).toHaveBeenLastCalledWith([409, JSON.stringify(findOneResult)])
     findOneResult = null
@@ -90,7 +90,7 @@ describe('createUser', () => {
       lastLoginDate: 'should be hidden',
     }
 
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
 
     const result = JSON.parse(next.mock.calls.pop()[0][1])
@@ -104,7 +104,7 @@ describe('createUser', () => {
   })
 
   it('Should return the new user id', async () => {
-    req.body = Object.assign({}, completeData)
+    req.body = Object.assign({}, data)
     await createUser(req, res, next)
     expect(res.send).toHaveBeenCalledWith({ _id: insertOneResult.insertedId })
   })
