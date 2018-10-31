36c030631487c1d63b816f157d32ce61801498e9
Mauro Constantinescu <mconst@mconst.it>
2018-10-31 12:47:50 +0100

    Refactoring.

--- user/endpoint.js
+++ user/endpoint.js
@@ -15,18 +15,15 @@ module.exports = ({
   validateUpdateUser,
   getJobRoles,
   catchExceptions
-}) => {
-  router
-  .post('/users/login', validateLogin, sendValidation, catchExceptions(login))
-  .use(loginGate)
-  .post('/users', validateCreateUser, sendValidation, catchExceptions(createUser))
-  .get('/users', catchExceptions(getUsers))
-  .get('/users/find', catchExceptions(findUsers))
-  .get('/users/roles', catchExceptions(getJobRoles))
-  .get('/users/:id', catchExceptions(getUser))
-  .put('/users/:id', validateUpdateUser, sendValidation, catchExceptions(updateUser))
-  .delete('/users/:id', catchExceptions(deleteUser))
-  .post('/users/logout', logout)
-
-  return router
-}
+}) =>
+router
+.post('/users/login', validateLogin, sendValidation, catchExceptions(login))
+.use(loginGate)
+.post('/users', validateCreateUser, sendValidation, catchExceptions(createUser))
+.get('/users', catchExceptions(getUsers))
+.get('/users/find', catchExceptions(findUsers))
+.get('/users/roles', catchExceptions(getJobRoles))
+.get('/users/:id', catchExceptions(getUser))
+.put('/users/:id', validateUpdateUser, sendValidation, catchExceptions(updateUser))
+.delete('/users/:id', catchExceptions(deleteUser))
+.post('/users/logout', logout)
