57a21121040f4a20809161c0b5b8d832ea20d3ff
Mauro Constantinescu <mconst@mconst.it>
2018-11-05 18:34:53 +0100

    Added project status.

---
+++ misc/status.js
@@ -0,0 +1,1 @@
+module.exports = ['opened', 'closed', 'cold', 'tbd']
--- project/make-validateCreateProject.test.js
+++ project/make-validateCreateProject.test.js
@@ -1,44 +1,50 @@
 const makeValidateCreateProject = require('./make-validateCreateProject')
 const check = require('../misc/test-expressValidator')
+const status = require('../misc/status')
 
 describe('validateCreateProject', () => {
   it('Should validate name', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('name', 'trim', 'not', 'isEmpty', 'isString', 'withMessage')
   })
 
   it('Should validate description', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('description', 'optional', 'trim', 'not', 'isEmpty', 'isString', 'withMessage')
   })
 
   it('Should validate people', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('people', 'optional', 'isArray', 'custom', 'withMessage')
   })
 
   it('Should validate people IDs', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('people.*._id', 'isMongoId', 'withMessage')
   })
 
   it('Should validate budget', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('budget', 'optional', 'trim', 'isNumeric', 'withMessage')
   })
 
   it('Should validate deadlines', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('deadlines', 'optional', 'isArray', 'withMessage')
   })
 
   it('Should validate deadlines dates', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('deadlines.*', 'optional', 'isISO8601', 'isAfter', 'withMessage')
   })
 
   it('Should validate terms', () => {
-    makeValidateCreateProject({ check })
+    makeValidateCreateProject({ check, status })
     check.validate('terms', 'not', 'exists', 'withMessage')
+  })
+
+  it('Should validate status', () => {
+    makeValidateCreateProject({ check, status })
+    check.validate('status', 'optional', 'isIn', 'isString', 'withMessage')
   })
 })
