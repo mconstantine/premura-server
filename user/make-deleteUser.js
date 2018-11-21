7676fa38d0f35fb37e0d8a9579ba26c6e1015960
Mauro Constantinescu <mconst@mconst.it>
2018-11-21 15:26:15 +0100

    deleteUser now handles "joins".

--- user/make-deleteUser.js
+++ user/make-deleteUser.js
@@ -11,8 +11,9 @@ module.exports = ({ createError, ObjectID, getDb, roles }) => async (req, res, n
   }
 
   const _id = new ObjectID(req.params.id)
-  const collection = (await getDb()).collection('users')
-  const user = await collection.findOne({ _id })
+  const db = await getDb()
+  const usersCollection = db.collection('users')
+  const user = await usersCollection.findOne({ _id })
   const currentUser = req.session.user
 
   if (!user) {
@@ -23,7 +24,65 @@ module.exports = ({ createError, ObjectID, getDb, roles }) => async (req, res, n
     return next(createError(401, 'only a master user can delete a user'))
   }
 
-  await collection.deleteOne({ _id })
+  const projectsCollection = db.collection('projects')
+  const projects = await projectsCollection.find({
+    'people._id': _id
+  }).toArray()
 
+  projects.forEach(async project => {
+    const people = project.people.reduce((res, person) => {
+      if (person._id.equals(_id)) {
+        return res
+      }
+
+      return res.concat([person])
+    }, [])
+
+    if (people.length) {
+      if (project.budget) {
+        const budgetPerPerson = project.budget / people.length
+        const budgetPerPersonInt = Math.floor(budgetPerPerson)
+
+        people.forEach(person => person.budget = budgetPerPerson)
+
+        if (budgetPerPersonInt < budgetPerPerson) {
+          people[0].budget++
+        }
+      }
+
+      await projectsCollection.updateOne({ _id: project._id }, {
+        $set: { people }
+      })
+    } else {
+      await projectsCollection.deleteOne({ _id: project._id })
+
+      const categoriesCollection = db.collection('categories')
+      const categories = await categoriesCollection.find({
+        'terms.projects': project._id
+      }).toArray()
+
+      categories.forEach(async category => {
+        category.terms.forEach(term => {
+          if (!term.projects.find(_id => _id.equals(project._id))) {
+            return
+          }
+
+          term.projects = term.projects.reduce((res, _id) => {
+            if (_id.equals(project._id)) {
+              return res
+            }
+
+            return res.concat([_id])
+          }, [])
+        })
+
+        await categoriesCollection.updateOne({ _id: category._id }, {
+          $set: { terms: category.terms }
+        })
+      })
+    }
+  })
+
+  await usersCollection.deleteOne({ _id })
   res.end()
 }
--- user/make-deleteUser.test.js
+++ user/make-deleteUser.test.js
@@ -4,14 +4,59 @@ const getDb = require('../misc/test-getDb')
 const ObjectID = require('../misc/test-ObjectID')
 
 describe('deleteUser', () => {
-  const _id = '1234567890abcdef'
-  const req = { params: { id: _id }, session: { user: { role: 'master' } } }
+  const id = '1234567890abcdef'
+  const req = { params: { id }, session: { user: { role: 'master' } } }
   const next = jest.fn()
   const createError = (httpCode, message) => [httpCode, message]
   const res = { status: jest.fn(() => res), send: jest.fn(), end: () => {} }
   const deleteUser = makeDeleteUser({ createError, getDb, ObjectID, roles })
 
-  getDb.setResult('findOne', { _id, role: 'maker' })
+  const getUser = () => ({
+    _id: new ObjectID(id),
+    role: 'maker'
+  })
+
+  const getProjects = () => [{
+    _id: new ObjectID('projectone'),
+    budget: 41,
+    people: [{
+      _id: new ObjectID(id),
+      budget: 41
+    }]
+  }, {
+    _id: new ObjectID('projecttwo'),
+    budget: 41,
+    people: [{
+      _id: new ObjectID(id),
+      budget: 21,
+    }, {
+      _id: new ObjectID('someoneelse'),
+      budget: 20,
+    }]
+  }]
+
+  const getCategories = () => [{
+    _id: new ObjectID('categoryone'),
+    terms: [{
+      _id: new ObjectID('termone'),
+      projects: [new ObjectID('projectone'), new ObjectID('projecttwo')]
+    }]
+  }]
+
+  const find = filters => {
+    if (filters['people._id']) {
+      return getProjects()
+    }
+
+    if (filters['terms.projects']) {
+      return getCategories()
+    }
+
+    return false
+  }
+
+  getDb.setResult('findOne', getUser)
+  getDb.setResult('find', find)
 
   it('Should check that an ID is provided', async () => {
     req.params = {}
@@ -26,18 +71,17 @@ describe('deleteUser', () => {
       }]
     })
 
-    req.params = { id: _id }
+    req.params = { id: id }
   })
 
   it('Should check for the user existence', async () => {
     getDb.functions.findOne.mockClear()
     await deleteUser(req, res, next)
-    expect(getDb.functions.findOne).toHaveBeenCalledWith({ _id: new ObjectID(_id) })
+    expect(getDb.functions.findOne).toHaveBeenCalledWith({ _id: new ObjectID(id) })
   })
 
   it('Should not allow a non master user to delete a user', async () => {
     req.session.user.role = 'manager'
-    getDb.setResult('findOne', { role: 'maker' })
     await deleteUser(req, res, next)
     expect(next).toHaveBeenCalledWith([401, expect.any(String)])
   })
@@ -46,12 +90,55 @@ describe('deleteUser', () => {
     next.mockClear()
     getDb.functions.deleteOne.mockClear()
     req.session.user.role = 'master'
-    getDb.setResult('findOne', { role: 'master' })
+    const user = getUser()
+    user.role = 'master'
+    getDb.setResult('findOne', user)
+    getDb.setResult('find', find)
     await deleteUser(req, res, next)
     expect(next).not.toHaveBeenCalled()
     expect(getDb.functions.deleteOne).toHaveBeenCalled()
+    getDb.setResult('findOne', getUser)
+    getDb.setResult('find', find)
   })
 
-  it.skip("Should remove user from projects' people", async () => {})
-  it.skip('Should remove projects that have this user as the only person', async () => {})
+  it("Should remove user from projects' people and redo budgets", async () => {
+    const projects = getProjects()
+    getDb.functions.updateOne.mockClear()
+    await deleteUser(req, res, next)
+
+    expect(getDb.functions.updateOne).toHaveBeenNthCalledWith(1, {
+      _id: projects[1]._id
+    }, {
+      $set: {
+        people: [{
+          _id: projects[1].people[1]._id,
+          budget: 41
+        }]
+      }
+    })
+  })
+
+  it(
+    'Should remove projects that have this user as the only person (also from terms)',
+    async () => {
+      const projects = getProjects()
+      const categories = getCategories()
+      getDb.functions.updateOne.mockClear()
+      getDb.functions.deleteOne.mockClear()
+      await deleteUser(req, res, next)
+      expect(getDb.functions.deleteOne).toHaveBeenCalledWith({
+        _id: projects[0]._id
+      })
+      expect(getDb.functions.updateOne).toHaveBeenCalledWith({
+        _id: categories[0]._id
+      }, {
+        $set: {
+          terms: [{
+            _id: categories[0].terms[0]._id,
+            projects: [categories[0].terms[0].projects[1]]
+          }]
+        }
+      })
+    }
+  )
 })
