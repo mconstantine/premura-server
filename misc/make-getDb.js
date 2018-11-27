c02a48907df70f2fd80160e9e5803b19b90d8664
Mauro Constantinescu <mconst@mconst.it>
2018-11-27 12:20:25 +0100

    Fixed getDb.

--- misc/make-getDb.js
+++ misc/make-getDb.js
@@ -1,11 +1,17 @@
-let db
+let db = null
 
 module.exports = ({ MongoClient, config }) => async () => {
   return db || (async () => {
     const { url, dbName } = config.db
     const client = await MongoClient.connect(`${url}/${dbName}`, { useNewUrlParser: true })
 
-    db = Object.assign(client.db(dbName), { close: client.close })
+    db = Object.assign(client.db(dbName), {
+      close: () => {
+        client.close()
+        db = null
+      }
+    })
+
     return db
   })()
 }
