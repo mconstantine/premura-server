29530087e8ce35ab41df803631811211a2a521c8
Mauro Constantinescu <mconst@mconst.it>
2018-11-29 13:04:48 +0100

    Removed useless unwinds from getProjectFromDb.

--- project/make-getProjectFromDb.js
+++ project/make-getProjectFromDb.js
@@ -2,8 +2,6 @@ module.exports = ({ sensitiveInformationProjection }) => async (db, _id) => {
   const projects = await db.collection('projects').aggregate([{
     $match: { _id }
   }, {
-    $unwind: '$people'
-  }, {
     $lookup: {
       from: 'users',
       localField: 'people._id',
@@ -11,24 +9,9 @@ module.exports = ({ sensitiveInformationProjection }) => async (db, _id) => {
       as: 'users'
     }
   }, {
-    $unwind: '$users'
-  }, {
     $project: Object.keys(sensitiveInformationProjection).reduce((res, key) => Object.assign(
       res, { [`users.${key}`]: sensitiveInformationProjection[key] }
     ), {})
-  }, {
-    $group: {
-      _id: '$_id',
-      people: { $push: '$people' },
-      users: { $push: '$users' },
-      name: { $first: '$name' },
-      description: { $first: '$description' },
-      deadlines: { $first: '$deadlines' },
-      status: { $first: '$status' },
-      budget: { $first: '$budget' },
-      creationDate: { $first: '$creationDate' },
-      lastUpdateDate: { $first: '$lastUpdateDate' }
-    }
   }])
   .toArray()
 
