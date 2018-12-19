91e124e8a75fb700915bf23bed6bb3c5584fd467
Mauro Constantinescu <mconst@mconst.it>
2018-12-19 12:45:51 +0100

    Validating project ID in addMessage.

--- project/make-validateAddMessage.js
+++ project/make-validateAddMessage.js
@@ -1,4 +1,7 @@
 module.exports = ({ check }) => [
+  check('id')
+  .isMongoId().withMessage('Invalid project ID'),
+
   check('content').trim()
   .not().isEmpty().withMessage('content is required')
   .isString().withMessage('content should be a String')
--- project/make-validateAddMessage.test.js
+++ project/make-validateAddMessage.test.js
@@ -2,6 +2,11 @@ const makeValidateAddMessage = require('./make-validateAddMessage')
 const check = require('../misc/test-expressValidator')
 
 describe('validateAddMessage', () => {
+  it('Should validate project ID', () => {
+    makeValidateAddMessage({ check })
+    check.validate('id', 'isMongoId', 'withMessage')
+  })
+
   it('Should validate content', () => {
     makeValidateAddMessage({ check })
     check.validate('content', 'trim', 'not', 'isEmpty', 'isString', 'withMessage')
